f2bd6ef8b81121499c1d2e891443ecf5627dd773
kashif.mehmood <kashismails@gmail.com>
2023-01-18 00:05:03 +0500

    Initial Commit

---
+++ SwiftUIThemingExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIThemingExample
+//
+//  Created by Kashif.Mehmood on 18/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIThemingExample/SwiftUIThemingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThemingExampleApp.swift
+//  SwiftUIThemingExample
+//
+//  Created by Kashif.Mehmood on 18/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThemingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
